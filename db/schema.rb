342d5f15b78d0b5dab6743a605c32096a8bf94e0
brad chick <brad@xacte.com>
2013-06-03 11:33:18 -0400

    adding a default for users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130323161751) do
+ActiveRecord::Schema.define(:version => 20130602185621) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id",                  :null => false
