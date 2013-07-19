5c489f65c7ca4e6a8857673d37a6f97a57609cca
bitsinmotion <bitsinmotion@gmail.com>
2013-07-18 22:35:16 -0700

    mail fixes

--- app/mailers/official_mailer.rb
+++ app/mailers/official_mailer.rb
@@ -1,7 +1,7 @@
 class OfficialMailer < ActionMailer::Base
   default from: "info@asq.us"
   
-  def poll_notification_mailer(user, quick_polls, rep_message)
+  def poll_notification_email(user, quick_polls, rep_message)
     @user = user
     @quick_polls = quick_polls
     @rep_message = rep_message
--- app/models/official_mailing.rb
+++ app/models/official_mailing.rb
@@ -14,20 +14,20 @@ class OfficialMailing < ActiveRecord::Base
     incumbent = Incumbent.find_by_official_id(official_id)
     
     unless incumbent.nil?
-       logger.debug "send_notification using incumbent " + incumbent.id.to_s
-       unless quick_polls.empty?
+       logger.debug "send_notification using office " + incumbent.office_id.to_s
+       unless self.quick_polls.empty?
         logger.debug "send_notification quick polls found"
         case incumbent.office_type_ukey
-          when OfficeType::US_SENATOR
+          when OfficeType::US_SENATOR.ukey
             logger.debug "send_notification incumbent is a US senator"
             users = User.where("rep_state_id = ?",incumbent.state_id)
-          when OfficeType::US_REP
+          when OfficeType::US_REP.ukey
             logger.debug "send_notification incumbent is a US representative"
             users = User.where("rep_state_id = ? and rep_congressional_district_no = ?", incumbent.state_id, incumbent.congressional_district_no)
-          when OfficeType::STATE_SENATOR
+          when OfficeType::STATE_SENATOR.ukey
             logger.debug "send_notification incumbent is a state senator"
             users = User.where("rep_state_id = ? and rep_state_senate_district_key = ?", incumbent.state_id, incumbent.state_senate_district_key)
-          when OfficeType::STATE_REP
+          when OfficeType::STATE_REP.ukey
             logger.debug "send_notification incumbent is a state representative"
             users = User.where("rep_state_id = ? and rep_state_house_district_key = ?", incumbent.state_id, incumbents.state_house_district_key)
           else
@@ -37,8 +37,8 @@ class OfficialMailing < ActiveRecord::Base
           logger.debug "send_notifications sending mail to user " + user.email
           OfficialMailer.poll_notification_email(user,quick_polls,rep_message).deliver
         end
-        official_mailing_status = MailingStatus::COMPLETE
-        save()                        
+        self.mailing_status= MailingStatus::COMPLETE
+        save!                     
       end
     end
        
