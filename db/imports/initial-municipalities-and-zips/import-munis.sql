ecb220e98a93cf9ca02b1ccf24bc0e5fa8246803
bitsinmotion <bitsinmotion@gmail.com>
2013-08-07 00:06:06 -0700

    fix minor glitch with import in last commit

--- db/imports/initial-municipalities-and-zips/import-munis.sql
+++ db/imports/initial-municipalities-and-zips/import-munis.sql
@@ -1,1 +1,1 @@
-\copy temp_imp_census_places(state_abbreviation, state_code, ansi_code, name, type, type_code, counties) from 'data/census-national-place-file.psv' delimiters '|' CSV;
+\copy temp_imp_census_places(state_abbreviation, state_code, ansi_code, name, place_type, type_code, counties) from 'data/census-national-place-file.psv' delimiters '|' CSV;
